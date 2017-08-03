3ce1dba01717285d586b4c049da04bd18226183a
Vadim <vadim.kolomoiets.unity@gmail.com>
2017-08-03 10:50:02 +0300

    MenuLauncher Backup

--- Assets/DevBranch/Scripts/Multiplayer/Launcher.cs
+++ Assets/DevBranch/Scripts/Multiplayer/Launcher.cs
@@ -6,169 +6,61 @@ using UnityEngine.UI;
 
 namespace Com.VadimUnityDev.Robots_vs_Zombies_VR
 {
-    public class Launcher : Photon.PunBehaviour
+  public class Launcher : Photon.PunBehaviour
+  {
+
+    #region Public Variables
+
+    /// <summary>
+    /// The PUN loglevel. 
+    /// </summary>
+    public PhotonLogLevel LogLevel = PhotonLogLevel.Informational;
+
+    [Tooltip("The Ui Panel to let the user enter name, connect and play")]
+    public GameObject controlPanel;
+    [Tooltip("The UI Label to inform the user that the connection is in progress")]
+    public GameObject progressLabel;
+    [Tooltip("The UI Dropdown which will select mode")]
+    public GameObject modeSelector;
+    [Tooltip("The UI Dropdown which will select map")]
+    public GameObject mapSelector;
+    [Tooltip("The UI GameObject group will be hidden/shown when needed")]
+    public GameObject beHidden;
+
+    #endregion
+
+    #region MonoBehaviour CallBacks
+    void Start()
     {
-
-        #region Public Variables
-
-        /// <summary>
-        /// The PUN loglevel. 
-        /// </summary>
-        public PhotonLogLevel LogLevel = PhotonLogLevel.Informational;
-
-        [Tooltip("The Ui Panel to let the user enter name, connect and play")]
-        public GameObject controlPanel;
-        [Tooltip("The UI Label to inform the user that the connection is in progress")]
-        public GameObject progressLabel;
-        [Tooltip("The UI Dropdown which will select mode")]
-        public GameObject modeSelector;
-        [Tooltip("The UI Dropdown which will select map")]
-        public GameObject mapSelector;
-        [Tooltip("The UI GameObject group will be hidden/shown when needed")]
-        public GameObject beHidden;
-
-        /// <summary>
-        /// The maximum number of players per room. When a room is full, it can't be joined by new players, and so new room will be created.
-        /// </summary>   
-        [Tooltip("The maximum number of players per room. When a room is full, it can't be joined by new players, and so new room will be created")]
-        public byte MaxPlayersPerRoom = 8;
-
-        #endregion
-
-        #region Private Variables
-
-        /// <summary>
-        /// This client's version number. Users are separated from each other by gameversion (which allows you to make breaking changes).
-        /// </summary>
-        private string _gameVersion = "1";
-
-        /// <summary>
-        /// Keep track of the current process. Since connection is asynchronous and is based on several callbacks from Photon, 
-        /// we need to keep track of this to properly adjust the behavior when we receive call back by Photon.
-        /// Typically this is used for the OnConnectedToMaster() callback.
-        /// </summary>
-        bool isConnecting;
-
-        #endregion
-
-        #region MonoBehaviour CallBacks
-
-        /// <summary>
-        /// MonoBehaviour method called on GameObject by Unity during early initialization phase.
-        /// </summary>
-
-        private void Awake()
-        {
-            // #Critical
-            // we don't join the lobby. There is no need to join a lobby to get the list of rooms.
-            PhotonNetwork.autoJoinLobby = false;
-
-            // #Critical
-            // this makes sure we can use PhotonNetwork.LoadLevel() on the master client and all clients in the same room sync their level automatically
-            PhotonNetwork.automaticallySyncScene = true;
-
-            // #NotImportant
-            // Force LogLevel
-            PhotonNetwork.logLevel = LogLevel;
-        }
-
-        /// <summary>
-        /// MonoBehaviour method called on GameObject by Unity during initialization phase.
-        /// </summary>
-        void Start()
-        {
-            progressLabel.SetActive(false);
-            beHidden.SetActive(true);
-        }
-
-        #endregion
-
-        #region Public Methods
-
-        /// <summary>
-        /// Start the connection process. 
-        /// - If already connected, we attempt joining a random room
-        /// - if not yet connected, Connect this application instance to Photon Cloud Network
-        /// </summary>
-        public void Connect()
-        {
-            // keep track of the will to join a room, because when we come back from the game we will get a callback that we are connected, so we need to know what to do then
-            isConnecting = true;
-
-            // we check if we are connected or not, we join if we are , else we initiate the connection to the server.
-            if (PhotonNetwork.connected)
-            {
-                // #Critical we need at this point to attempt joining a Random Room. If it fails, we'll get notified in OnPhotonRandomJoinFailed() and we'll create one.
-                PhotonNetwork.JoinRandomRoom();
-            }
-            else
-            {
-                // #Critical, we must first and foremost connect to Photon Online Server.
-                PhotonNetwork.ConnectUsingSettings(_gameVersion);
-            }
-
-            progressLabel.SetActive(true);
-            beHidden.SetActive(false);
-        }
-
-        #endregion
-
-        #region Photon.PunBehaviour CallBacks
-
-        public override void OnConnectedToMaster()
-        {
-            Debug.Log("DemoAnimator/Launcher: OnConnectedToMaster() was called by PUN");
-
-            // we don't want to do anything if we are not attempting to join a room. 
-            // this case where isConnecting is false is typically when you lost or quit the game, when this level is loaded, OnConnectedToMaster will be called, in that case
-            // we don't want to do anything.
-            if (isConnecting)
-            {
-                // #Critical: The first we try to do is to join a potential existing room. If there is, good, else, we'll be called back with OnPhotonRandomJoinFailed()  
-                PhotonNetwork.JoinRandomRoom();
-            }
-        }
-
-        public override void OnDisconnectedFromPhoton()
-        {
-            progressLabel.SetActive(false);
-            beHidden.SetActive(true);
-
-            Debug.LogWarning("DemoAnimator/Launcher: OnDisconnectedFromPhoton() was called by PUN");
-        }
-
-        public override void OnPhotonRandomJoinFailed(object[] codeAndMsg)
-        {
-            Debug.Log("DemoAnimator/Launcher:OnPhotonRandomJoinFailed() was called by PUN. No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom(null, new RoomOptions() {maxPlayers = 4}, null);");
-
-            // #Critical: we failed to join a random room, maybe none exists or they are all full. No worries, we create a new room.
-            PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = MaxPlayersPerRoom }, null);
-        }
-
-        public override void OnJoinedRoom()
-        {
-            Debug.Log("DemoAnimator/Launcher: OnJoinedRoom() called by PUN. Now this client is in a room.");
-            progressLabel.GetComponent<Text>().text = "Connected!";
-
-            // #Critical: We only load if we are the first player, else we rely on  PhotonNetwork.automaticallySyncScene to sync our instance scene.
-
-            // #Critical: Here will be map checking
-            if (PhotonNetwork.room.playerCount == 1 && mapSelector.GetComponent<Dropdown>().value == 0)
-            {
-                Debug.Log("We load the 'Down Town' map");
-                // #Critical
-                //Load the Room Down Town level
-                PhotonNetwork.LoadLevel(1);
-            }
-            else if (PhotonNetwork.room.playerCount == 1 && mapSelector.GetComponent<Dropdown>().value == 1)
-            {
-                Debug.Log("We load the 'Old town' map");
-                // #Critical
-                //Load the Room Old Town level
-                PhotonNetwork.LoadLevel(2);
-            }
-        }
-
-        #endregion
+      progressLabel.SetActive(false);
+      beHidden.SetActive(true);
     }
+
+    #endregion
+
+    #region Public Methods
+
+    public void Connect()
+    {
+      progressLabel.SetActive(true);
+      beHidden.SetActive(false);
+
+      if (mapSelector.GetComponent<Dropdown>().value == 0)
+      {
+        Debug.Log("We load the 'Down Town' map");
+        // #Critical
+        //Load the Room Down Town level
+        PhotonNetwork.LoadLevel(1);
+      }
+      else if (mapSelector.GetComponent<Dropdown>().value == 1)
+      {
+        Debug.Log("We load the 'Old town' map");
+        // #Critical
+        //Load the Room Old Town level
+        PhotonNetwork.LoadLevel(2);
+      }
+    }
+
+    #endregion
+  }
 }
